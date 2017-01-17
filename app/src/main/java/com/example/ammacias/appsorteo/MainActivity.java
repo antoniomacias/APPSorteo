c10327d6529cc3a4151440a443829b9bd3d639e9
antmacmun <antmacmun@gmail.com>
2017-01-17 11:53:39 +0100

    Options menu

--- app/src/main/java/com/example/ammacias/appsorteo/MainActivity.java
+++ app/src/main/java/com/example/ammacias/appsorteo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.ammacias.appsorteo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Toast.makeText(this, "Ajustes", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_logout:
+                Toast.makeText(this, "Cerrar sesión", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
